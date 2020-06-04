601ce5ea5f62a54b08732389532a84e3f61df1ff
Eli Cusic <tntitans31@gmail.com>
2020-06-04 20:18:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var playlist = {
   RATM: "Maggie's Farm"
 }
 
-var updatePlaylist = Object.assign(playlist, {DMX: "X gonna give to ya"})
+var updatePlaylist = Object.assign(playlist, artistName, songTitle)
